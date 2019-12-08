c2071ea137cce19e4076f96c4ae429b502640ebc
Kate Dameron <katerj33@gmail.com>
2019-12-08 15:33:15 -0800

    WIP working on filling in missing tds from rowspan attrs

--- scraper.js
+++ scraper.js
@@ -2,16 +2,30 @@ const request = require('superagent');
 const { parse } = require('node-html-parser');
 
 const gimmeTheData = html => {
-  const title = html.querySelectorAll('h3 .mw-headline')
+  const tableType = html.querySelectorAll('h3 .mw-headline')
     .map(node => node.rawText);
-  const tables = html.querySelectorAll('.wikitable')
-    .map(node => node.rawText)[1];
-    // .map(any => typeof any);
-  // const keys = tables.querySelectorAll('tr')
-  //   .map(node => node.rawText)[0];
-  
-  return tables;
-  // .map(node => node);
+
+  const rowColAttrs = html.querySelectorAll('td')
+    .map(node => node.attributes);
+
+  const allRows = html.querySelectorAll('tr')
+    .map(node => node.rawText
+      .replace('\n', '')
+      .split('\n')
+      .filter(str => Boolean(str)));
+
+  const tableTitles = allRows.filter(row => row[0] === 'Title');
+  const data = allRows.filter(row => row[0] !== 'Title');
+
+  const rowSpanData = rowColAttrs.filter((att, i) => {
+      if (att.hasOwnProperty('rowspan')) {
+        const flatData = data.flat();
+        att.value = flatData[i];
+        att.startsAtIndex = i;
+        return att;
+      }
+  });
+  console.log(rowSpanData);
 };
   
 const scraper = () => {
