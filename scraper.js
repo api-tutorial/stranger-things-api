562e41bd195a34deafa863afba872de425a4110e
Paige Gorry <paigeegorry@gmail.com>
2020-01-18 14:29:47 -0800

    WE HAVE THE RIGHT ROWSPAN, KEY VALUE PAIRS

--- scraper.js
+++ scraper.js
@@ -3,29 +3,33 @@ const { parse } = require('node-html-parser');
 
 const gimmeTheData = html => {
   const tableType = html.querySelectorAll('h3 .mw-headline')
-    .map(node => node.rawText);
+    .map(node => node.rawText); // titles of table, outside table
 
   const rowColAttrs = html.querySelectorAll('td')
-    .map(node => node.attributes);
+    .map(node => ({ ...node.attributes, value: node.rawText})); // each table cell
 
-  const allRows = html.querySelectorAll('tr')
+  console.log('what', rowColAttrs)
+
+  const allRows = html.querySelectorAll('tr') // each table row
     .map(node => node.rawText
       .replace('\n', '')
       .split('\n')
       .filter(str => Boolean(str)));
 
-  const tableTitles = allRows.filter(row => row[0] === 'Title');
-  const data = allRows.filter(row => row[0] !== 'Title');
-
-  const rowSpanData = rowColAttrs.filter((att, i) => {
-      if (att.hasOwnProperty('rowspan')) {
-        const flatData = data.flat();
-        att.value = flatData[i];
-        att.startsAtIndex = i;
-        return att;
+  const tableTitles = allRows.filter(row => row[0] === 'Title'); // titles from tables TH
+  const data = allRows.filter(row => row[0] !== 'Title'); // all rows, NOT including table header rows 
+  
+  let typeIndex = -1;
+  const typeAndTitle = allRows.map((row) => {
+    if(row[0] === 'Title') typeIndex++;
+    else {
+      return {
+        title: row[0],
+        type: tableType[typeIndex]
       }
-  });
-  console.log(rowSpanData);
+    }
+  }).filter(obj => obj instanceof Object)
+  // console.log('WHAT', typeAndTitle)
 };
   
 const scraper = () => {
@@ -34,7 +38,6 @@ const scraper = () => {
     .then(res => res.text)
     .then(parse)
     .then(gimmeTheData)
-    .then(console.log);
 };
 
 scraper();
