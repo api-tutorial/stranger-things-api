8529fc9296b824f6b6fa31452ecbb7b19b3eb444
Paige Gorry <paigeegorry@gmail.com>
2020-02-14 20:39:56 -0800

    initial scraping working; committing before refactor

--- infoScraper.js
+++ infoScraper.js
@@ -7,55 +7,55 @@ const removeSymbol = arr => arr.map(str => str.replace(' †', ''))
 const removeHtmlTags = str => str.replace('<small>', '').replace('</small>', '').replace('<p>', '').replace('</p>', '')
 
 const reformatData = ({ labels, values, photoInfo, name }) => {
-  const obj = {};
-  obj.photo = photoInfo;
-  obj.name = name;
-  
-  labels.map((l, i) => {
-    console.log(values[i].text)
-    const label = lodash.camelCase(l)
-    const value = values[i].text
-    const htmlVal = values[i].innerHTML
-    if(label === 'appearsInEpisodes') {
-      let newVal = value.trim().split(' ')
-      obj[label] = newVal.length > 0 ? newVal : newVal[0]
-    }
-    else if(htmlVal.includes('<br />')) {
-      if(label === 'affiliation') {
-        let newVal = htmlVal.split('>').map(str => str.trim())
-          .filter(str => str[0] !== '<' && str[0] !== '/' && str.length > 0 && !str.includes('(formerly)'))
-          .map(str => str.replace('</a', '').replace('</small', ''))
-        newVal = newVal.map(str => {
-          if(str.includes('<a href')) return
-          else return str
-        })
-        obj[label] = newVal.filter(s => s)
+  if(labels.length === values.length) {
+    const obj = {};
+    obj.photo = photoInfo;
+    obj.name = name;
+    labels.map((l, i) => {
+      const label = lodash.camelCase(l)
+      const value = values[i].text
+      const htmlVal = values[i].innerHTML
+      if(label === 'appearsInEpisodes') {
+        let newVal = value.trim().split(' ')
+        obj[label] = newVal.length > 0 ? newVal : newVal[0]
       }
-      else if(!htmlVal.includes('<a href')) {
-        const newVal = htmlVal.split('<br />')
-          .filter(s => s).map(s => removeHtmlTags(s))
-        obj[label] = removeSymbol(newVal)
+      else if(htmlVal.includes('<br />')) {
+        if(label === 'affiliation') {
+          let newVal = htmlVal.split('>').map(str => str.trim())
+            .filter(str => str[0] !== '<' && str[0] !== '/' && str.length > 0 && !str.includes('(formerly)'))
+            .map(str => str.replace('</a', '').replace('</small', ''))
+          newVal = newVal.map(str => {
+            if(str.includes('<a href')) return
+            else return str
+          })
+          obj[label] = newVal.filter(s => s)
+        }
+        else if(!htmlVal.includes('<a href')) {
+          const newVal = htmlVal.split('<br />')
+            .filter(s => s).map(s => removeHtmlTags(s))
+          obj[label] = removeSymbol(newVal)
+        }
       }
-    }
-    else if(value.includes(')') && label !== 'height') {
-      let newVal = value.split(')').map(s => s.length > 0 ? (s + ')').trim() : undefined).filter(s => s && s !== ')')
-      if(label === 'residence') {
-        newVal = newVal.map(s => s.replace(': ', '').trim())
+      else if(value.includes(')') && label !== 'height') {
+        let newVal = value.split(')').map(s => s.length > 0 ? (s + ')').trim() : undefined).filter(s => s && s !== ')')
+        if(label === 'residence') {
+          newVal = newVal.map(s => s.replace(': ', '').trim())
+        }
+        newVal = removeSymbol(newVal)
+        obj[label] = newVal.length === 1 ? newVal[0] : newVal
       }
-      newVal = removeSymbol(newVal)
-      obj[label] = newVal.length === 1 ? newVal[0] : newVal
-    }
-    else {
-      obj[label] = value
-    }
-  });
+      else {
+        obj[label] = value
+      }
+    });
+    return obj;
+  }
+  else undefined
 
-  return obj;
 };
 
 const infoScraper = async() => {
-  // const names = await nameScraper()
-  const names = ['Tom Holloway']
+  const names = await nameScraper()
 
   try {
     return Promise.all(
@@ -64,18 +64,20 @@ const infoScraper = async() => {
         .then(res => res.text)
         .then(parse)
         .then(html => {
-          const labels = html.querySelectorAll('.pi-data-label').map(l => l.structuredText);
-          const values = html.querySelectorAll('div .pi-data-value');
-          const photoInfo = html.querySelectorAll('.pi-image-thumbnail').length ? html.querySelectorAll('.pi-image-thumbnail')[0].rawAttrs.split('"')[1] : 'https://pbs.twimg.com/profile_images/514121481702227968/XxIE7ASP_400x400.jpeg';
-          return { labels, values, photoInfo, name };
+          const labels = html.querySelectorAll('.pi-data-label').map(l => l.structuredText)
+          const values = html.querySelectorAll('div .pi-data-value')
+          const photoInfo = html.querySelectorAll('.pi-image-thumbnail').length 
+            ? html.querySelectorAll('.pi-image-thumbnail')[0].rawAttrs.split('"')[1] 
+            : 'https://upload.wikimedia.org/wikipedia/commons/3/38/Stranger_Things_logo.png' // default logo image
+          return { labels, values: values.filter(v => v.text), photoInfo, name };
         })
         .then(reformatData)
         .then(console.log)
-        .catch(err => console.log({err, name}));
+        .catch(err => console.log({ name, err }))
       }))
     }
   catch(err) {
-    console.error(err);
+    console.error(err)
   }
 };
 
