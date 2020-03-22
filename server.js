6d4c1e3944c4b1cf9dff0dd46dc99c0327c563ce
Paige Gorry <paigeegorry@gmail.com>
2020-03-22 14:39:35 -0700

    bye bye chalk

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
-const chalk = require('chalk');
 const app = require('./lib/app');
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(chalk.greenBright(`listening at PORT ${PORT}`));
+  console.log(`listening at PORT ${PORT}`);
 });
