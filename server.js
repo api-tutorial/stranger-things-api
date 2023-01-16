709a30c1b581017140887f0d7dc22a61bb51e502
Paige Gorry <paigeegorry@gmail.com>
2023-01-15 18:02:57 -0800

    add redirect to netlify for api documentation

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = require('./lib/app');
 
 const PORT = process.env.PORT || 8080;
 
+app.get('/', function(req, res){
+  res.redirect('https://strangerthingsapi.netlify.app/docs');
+});
+
 app.listen(PORT, () => {
   console.log(`listening at PORT ${PORT}`);
 });
